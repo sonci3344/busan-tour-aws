bbcd308689c9eaa1f5cb1dba6470942e2a8c3106
CDNG <107281943+Hyunkyoungkang@users.noreply.github.com>
2024-11-12 14:13:53 +0900

    Update FileController.java

--- project_DiB/src/main/java/org/zerock/project_dib/restaurant/controller/FileController.java
+++ project_DiB/src/main/java/org/zerock/project_dib/restaurant/controller/FileController.java
@@ -16,7 +16,7 @@ import java.nio.file.Files;
 public class FileController {
 
     @Value("${org.zerock.upload.path}")
-    private String uploadPath;
+    private String uploadPath = /var/upload;
 
     @GetMapping("/view/{fileName}")
     public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
